944bed9659d590df757c34a0524c69a366629aaf
HSR <hseoglan@naver.com>
2026-03-13 17:35:45 +0900

    3_13오늘의 끝(2차)

--- src/main/java/com/example/demo/domain/Member.java
+++ src/main/java/com/example/demo/domain/Member.java
@@ -1,9 +1,7 @@
 package com.example.demo.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -11,16 +9,25 @@ import java.time.LocalDateTime;
 
 @Entity
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
-    private String email;
+
+    @Column(unique = true, nullable = false)
+    String email;
+
+    @Column(nullable = false)
     private String password;
+
+    @Column(nullable = false)
     private String role;
+
+    private String refreshToken;
 
     private LocalDateTime createdAt;
 
@@ -36,5 +43,9 @@ public class Member {
     public void update(String name, String email){
         this.name=name;
         this.email=email;
+    }
+
+    public void updateRefreshRoken(String refreshToken) {
+        this.refreshToken=refreshToken;
     }
 }
--- src/main/java/com/example/demo/dto/MemberResponse.java
+++ src/main/java/com/example/demo/dto/MemberResponse.java
@@ -11,12 +11,14 @@ public class MemberResponse {
     private Long id;
     private String name;
     private String email;
+    private String role;
     private LocalDateTime createdAt;
 
     public MemberResponse(Member member){
         this.id = member.getId();
         this.name=member.getName();
         this.email=member.getEmail();
+        this.role=member.getRole();
         this.createdAt= member.getCreatedAt();
     }
 }
